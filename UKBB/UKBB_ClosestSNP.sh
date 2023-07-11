40e0c11a991def7babfd598ea41c147e71af1d52
mayukh <mondal.mayukh@gmail.com>
2023-07-11 12:58:32 +0200

    UKBB_Closest.sh works

--- UKBB/UKBB_ClosestSNP.sh
+++ UKBB/UKBB_ClosestSNP.sh
@@ -1,5 +1,6 @@
-tabix -R $1 $2 | cut -f 1,2 | awk '{print $1"\t"$2-1"\t"$2}' > Present.bed
-cat <(cut -f 1,3 topsnp_gr37.bed ) <(cut -f 1,3 Present.bed ) | sort | uniq -c | awk '{print $1"\t"$2"\t"$3}' | grep -vw "^2" | cut -f 2,3 > notpresent.snps
-cat notpresent.snps | awk -v q="'" -v q=$1 '{print "tabix "q" "$1":"$2-1000"-"$2+1000" | cut -f 2| awk "q"{print \""$1"\\t\"$1-1\"\\t\"$1\"\\t\"($1-"$2")^2}"q"| sort -k 4,4n|head -n 1 | cut -f -3"}' > closest.sh
-sh closest.sh > closest.bed
-cat Present.bed closest.bed| sort -k1,1n -k2,2  > All.bed
+mkdir -p temp
+tabix -R $1 $2 | cut -f 1,2 | awk '{print $1"\t"$2-1"\t"$2}' > temp/Present.bed
+cat <(cut -f 1,3 $1 ) <(cut -f 1,3 temp/Present.bed ) | sort | uniq -c | awk '{print $1"\t"$2"\t"$3}' | grep -vw "^2" | cut -f 2,3 > temp/notpresent.snps
+cat temp/notpresent.snps | awk -v q="'" -v f=$2 '{print "tabix "f" "$1":"$2-1000"-"$2+1000" | cut -f 2| awk "q"{print \""$1"\\t\"$1-1\"\\t\"$1\"\\t\"($1-"$2")^2}"q"| sort -k 4,4n|head -n 1 | cut -f -3"}' > temp/closest.sh
+sh temp/closest.sh > temp/closest.bed
+cat temp/Present.bed temp/closest.bed| sort -k1,1n -k2,2
